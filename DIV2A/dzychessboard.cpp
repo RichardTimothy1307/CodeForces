d7fd11885837465192b9489d4f01c3ccb4b60a1c
RichardTimothy1307 <richardtimothy82@gmail.com>
2021-11-28 19:17:37 +0530

    chess board

---
+++ DIV2A/dzychessboard.cpp
@@ -0,0 +1,68 @@
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+//double pi = 3.1415926536;
+#define  PB push_back
+#define  MP make_pair
+const int mod = 1e7;
+//vector<int> graph[(int)1000];  //defining array of vectors
+
+
+
+
+
+void solve() {
+
+	int n, m;
+	cin >> n >> m;
+	int b = 0;
+	int w = 1;
+
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		for (int j = 0; j < m; j++) {
+			if (s[j] == '.') {
+				if ((i + j) % 2 == 0) {
+					s[j] = 'B';
+				}
+				else {
+					s[j] = 'W';
+				}
+
+			}
+
+			//cout << endl;
+		}
+		cout << s << endl;
+
+	}
+}
+
+
+
+
+
+
+
+signed main() {
+#ifndef ONLINE_JUDGE
+	freopen("ccallinput.txt", "r", stdin);
+	freopen("ccalloutput.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+
+	return 0;
+
+
+}
